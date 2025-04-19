4803e2c27327204533e7ae0bde30509b1ed31483
wanjohi lewi <wanjohilewi7@gmail.com>
2025-04-19 13:57:43 +0300

    made changes to auth

--- Auth/src/main/java/com/mycompany/app/repository/RoleRepository.java
+++ Auth/src/main/java/com/mycompany/app/repository/RoleRepository.java
@@ -2,11 +2,10 @@ package com.mycompany.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.management.relation.Role;
+import com.mycompany.app.model.Role;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(com.mycompany.app.model.Role.ERole name);
+    Optional<Role> findByName(Role.ERole name);
 }
