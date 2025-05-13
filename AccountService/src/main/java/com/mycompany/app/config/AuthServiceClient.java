042f1a59de922f7fd8d5bb18e534c5813508a2f4
wanjohi lewi <wanjohilewi7@gmail.com>
2025-05-13 21:36:44 +0300

    made changes in the AccountModule

--- AccountService/src/main/java/com/mycompany/app/config/AuthServiceClient.java
+++ AccountService/src/main/java/com/mycompany/app/config/AuthServiceClient.java
@@ -1,6 +1,7 @@
 package com.mycompany.app.config;
 
 import com.mycompany.app.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
